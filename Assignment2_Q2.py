72c0cbc3e796fb52abe8e55d6103583d5e47185f
Sandeep Gautam <sandeepgautam568@gmail.com>
2026-01-14 22:52:57 +1100

    added references to the python code

--- Assignment2_Q2.py
+++ Assignment2_Q2.py
@@ -1,6 +1,20 @@
-import numpy as np
-import pandas as pd
+# Create a program that analyses temperature data collected from multiple weather stations in Australia. The data is stored in multiple CSV files under a "temperatures" folder, with each file representing data from one year. Process ALL .csv files in the temperatures folder. Ignore missing temperature values (NaN) in calculations.
+# Main Functions to Implement:
+# 1. Seasonal Average: Calculate the average temperature for each season across ALL stations and ALL years. Save the results to "average_temp.txt".
+#   • Use Australian seasons: Summer (Dec-Feb), Autumn (Mar-May), Winter (Jun- Aug), Spring (Sep-Nov)
+#   • Output format example: "Summer: 28.5°C"
+# 2. Temperature Range: Find the station(s) with the largest temperature range (difference between the highest and lowest temperature ever recorded at that station). Save the results to "largest_temp_range_station.txt".
+#   • Output format example: "Station ABC: Range 45.2°C (Max: 48.3°C, Min: 3.1°C)"
+#   • If multiple stations tie, list all of them
+# 3. Temperature Stability: Find which station(s) have the most stable temperatures
+# (smallest standard deviation) and which have the most variable temperatures (largest
+# standard deviation). Save the results to "temperature_stability_stations.txt".
+#   • Output format example:
+#       o "Most Stable: Station XYZ: StdDev 2.3°C"
+#       o "Most Variable: Station DEF: StdDev 12.8°C"
+#   • If multiple stations tie, list all of them.
 
+import pandas as pd
 
 # Listing all files to be read
 files = [
@@ -27,48 +41,50 @@ files = [
 ]
 
 all_data = []
-for file in files:             # Reading all csv files at once
-    df = pd.read_csv(file)
-    all_data.append(df)
 
+ # Reading all csv files at once, and making a massive dataframe of the data
+
+for file in files:
+    df = pd.read_csv(file) #[4]
+    all_data.append(df)
 
 months = ["January","February","March","April","May","June", "July","August","September","October","November","December"]
 
-
-full_df = pd.concat(all_data) # Merging all dataframes into one to find seasonal averages/temperature range/most stable and variable stations across all years
-
+# Merging all dataframes into one to find seasonal averages/temperature range/most stable and variable stations across all years
+full_df = pd.concat(all_data)
 
 
 ## SEASONAL AVERAGES ##
 
-summer = full_df[["December", "January", "February"]].values.flatten()  # Taking numbers from these months and then flatten them into 1d array so that we can calcuate mean value
+# Taking numbers from these months and then flatten them into 1d array so that we can calcuate mean value. Only taking seasons into consideration
+summer = full_df[["December", "January", "February"]].values.flatten() #[5]
 autumn = full_df[["March", "April", "May"]].values.flatten()
 winter = full_df[["June", "July", "August"]].values.flatten()
 spring = full_df[["September", "October", "November"]].values.flatten()
 
+# Average of each season
 summer_avg = summer.mean()
 autumn_avg = autumn.mean()
 winter_avg = winter.mean()
 spring_avg = spring.mean()
 
-
-with open("average_temp.txt", "w") as f:         # Writing the seasonal averages to a text file
+ # Writing the seasonal averages to a text file
+with open("average_temp.txt", "w") as f:
     f.write(f"Summer: {round(summer_avg,2)}°C\n")
     f.write(f"Autumn: {round(autumn_avg,2)}°C\n")
     f.write(f"Winter: {round(winter_avg,2)}°C\n")
     f.write(f"Spring: {round(spring_avg,2)}°C\n")
-    
+
 
 
 ## STATION WITH LARGEST TEMPERATURE RANGE ##
-
-maxi_temp = full_df.groupby("STATION_NAME")[months].max().max(axis=1) # max temperature for all stations in each month, then takes overall max for each station)
-mini_temp = full_df.groupby("STATION_NAME")[months].min().min(axis=1) # min temperature for all stations in each month, then takes overall min for each station)
+maxi_temp = full_df.groupby("STATION_NAME")[months].max().max(axis=1) #[6] # max temperature for all stations in each month, then takes overall max for each station
+mini_temp = full_df.groupby("STATION_NAME")[months].min().min(axis=1) # min temperature for all stations in each month, then takes overall min for each station
 
 tempeature_range = maxi_temp - mini_temp
 
 largest_range = tempeature_range.max()
-top_stations = tempeature_range[tempeature_range == largest_range] # only stations with the largest range (in case of ties)
+top_stations = tempeature_range[tempeature_range == largest_range]  # only stations with the largest range (in case of equal values)
 
 with open("largest_temp_range_station.txt", "w") as f:
     for stations in top_stations.index:
@@ -81,11 +97,10 @@ with open("largest_temp_range_station.txt", "w") as f:
 
 
 ## MOST STABLE AND MOST VARIABLE STATIONS ##
+std_dev = full_df.groupby("STATION_NAME")[months].std().mean(axis=1) # [7] standard deviation for each month per station, then average these std devs to get variability)
 
-std_dev = full_df.groupby("STATION_NAME")[months].std().mean(axis=1)  # standard deviation for each month per station, then average these std devs to get variability)
-
-most_stable_value = std_dev.min()       #smallest and largest std dev values
-most_variable_value = std_dev.max()
+most_stable_value = std_dev.min()   #smallest and largest std dev values
+most_variable_value = std_dev.max() # [7]
 
 most_stable_stations = std_dev[std_dev == most_stable_value]  # handles ties stations with most stable and most variable values
 most_variable_stations = std_dev[std_dev == most_variable_value]
--- encryption.py
+++ encryption.py
@@ -1,3 +1,26 @@
+# Create a program that reads the text file "raw_text.txt", encrypts its contents using a simple encryption method, and writes the encrypted text to a new file"encrypted_text.txt". Then create a function to decrypt the content and a function to verify the decryption was successful.
+# Requirements
+# The encryption should take two user inputs (shift1, shift2), and follow these rules:
+# • For lowercase letters:
+#   o If the letter is in the first half of the alphabet (a-m): shift forward by shift1 * shift2 positions
+#   o If the letter is in the second half (n-z): shift backward by shift1 + shift2 positions
+# • For uppercase letters:
+#   o If the letter is in the first half (A-M): shift backward by shift1 positions
+#   o If the letter is in the second half (N-Z): shift forward by shift2² positions (shift2 squared)
+# • Other characters:
+#   o Spaces, tabs, newlines, special characters, and numbers remain unchanged
+# Main Functions to Implement
+# 1. Encryption function: Reads from "raw_text.txt" and writes encrypted content to "encrypted_text.txt" .
+# 2. Decryption function: Reads from "encrypted_text.txt" and writes the decrypted content to "decrypted_text.txt" .
+# 3. Verification function: Compares "raw_text.txt" with "decrypted_text.txt" and prints whether the decryption was successful or not.
+# Program Behavior
+# When run, your program should automatically:
+# 1. Prompt the user for shift1 and shift2 values
+# 2. Encrypt the contents of "raw_text.txt"
+# 3. Decrypt the encrypted file
+# 4. Verify the decryption matches the original
+# (Caesar Ciper's modification) [10]
+
 #Accept shift 1 and Shift 2 numbers from the user for encryption
 shift1 = int(input("Enter a shift1 number: "))
 shift2 = int(input("Enter a shift2 number: "))
@@ -19,9 +42,16 @@ with open(input_file, "r") as file:
                 # First holding the original character to be later added to the "raw_text_file_content"
                 content += char
                 # Perform encryption operation based on the alphabets a-m, n-z, A-M and N-Z using shift1 and shift2
+                #
+                # ord() function takes in a unicode character and returns the ascii value. the ascii value is the number
+                # on which we can operate, since these values are much more easier to perform the rotational calculation on
+                # instead of counting the number of characters, in an array for example. We do the math directly instead.
+                #
+                # The modulo by 13 is important because we're operating on first and second half of 26 characters.
+                # At the end of operation, we convert the ascii number back to character with chr().
                 if 'a' <= char <= 'm':
-                    number = ord(char) - ord('a') + (shift1 * shift2)
-                    newline += chr((number % 13) + ord('a'))
+                    number = ord(char) - ord('a') + (shift1 * shift2) # [8]
+                    newline += chr((number % 13) + ord('a')) # [9]
                 elif 'n' <= char <= 'z':
                     number = ord(char) - ord('n') - (shift1 + shift2)
                     newline += chr(number % 13 + ord('n'))
@@ -50,7 +80,7 @@ write_decrypted_file = open("decrypted_text.txt","w")
 # String to store the contents after decryption
 decrypted_file_content = ""
 
-# Read the encrypted file
+# Reading the encrypted file
 encrypted_file = "encryption_text.txt"
 with open("encryption_text.txt", "r") as file:
     try:
@@ -88,9 +118,3 @@ if raw_text_file_content==decrypted_file_content:
     print("Decrypted text matches the original text")
 else:
     print("Decrypted text does not match the original text")
-
-
-
-
-
-
